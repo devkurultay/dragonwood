b57741f4cbf45b0d550a9a42f1ab652950859a48
muratjumashev <jumasheff@gmail.com>
2020-03-29 16:53:59 +0600

    Add createAdventurerCards helper

---
+++ gameplay/helpers.ts
@@ -0,0 +1,14 @@
+import { AdventurerCard } from "../entities/advernture_cards";
+import { Color } from "../entities/advernture_cards";
+
+export function createAdventurerCards (): Array<AdventurerCard> {
+    const colors = Object.keys(Color)
+    const numbers = Array.from(Array(12).keys())
+    const cards: Array<AdventurerCard> = []
+    numbers.forEach(num => { 
+        colors.forEach(color => {
+            cards.push(new AdventurerCard(color, num))
+        })
+    })
+    return cards
+}
