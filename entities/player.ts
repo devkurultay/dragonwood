988ce79a857f7cd77b672dbd69e76e14e591ceb3
muratjumashev <jumasheff@gmail.com>
2020-03-31 19:45:23 +0600

    Add dragonWoodDeck and addCardToDragonWoodDeck

--- entities/player.ts
+++ entities/player.ts
@@ -1,8 +1,15 @@
 import { AdventurerCard } from "./advernture_cards";
+import { DragonWoodCard } from "./dragonwood_cards";
 
 export class Player {
-    deck: Array<AdventurerCard> = [];
+    deck: Array<AdventurerCard> = []
+    dragonWoodDeck: Array<DragonWoodCard> = []
+
     addCardToDeck(adventurerCard: AdventurerCard) {
         this.deck.push(adventurerCard)
+    }
+
+    addCardToDragonWoodDeck(dragonWoodCard: DragonWoodCard) {
+        this.dragonWoodDeck.push(dragonWoodCard)
     }
 }
